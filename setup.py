5077e8860e41a6486add9c4b418fc00ad7ebac1d
Christopher Kobayashi <software+android@disavowed.jp>
2020-02-24 11:26:12 +0900

    Nuke requires from orbit.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ plugin_url = "https://github.com/christopherkobayashi/octoprint-LCDd"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = [ None ]
+# plugin_requires = [ None ]
 
 ### ------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
@@ -92,7 +92,7 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
     mail=plugin_author_email,
     url=plugin_url,
     license=plugin_license,
-    requires=plugin_requires,
+#    requires=plugin_requires,
     additional_packages=plugin_additional_packages,
     ignored_packages=plugin_ignored_packages,
     additional_data=plugin_additional_data
