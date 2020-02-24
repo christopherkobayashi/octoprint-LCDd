e57a7bfebd376c030c5ecc093582bad61c462c3d
Christopher Kobayashi <software+android@disavowed.jp>
2020-02-24 11:19:37 +0900

    Hooray, python whitespace.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ plugin_url = "https://github.com/christopherkobayashi/octoprint-LCDd"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
- plugin_requires = []
+plugin_requires = []
 
 ### ------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
